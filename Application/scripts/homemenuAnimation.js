8de7243f6d3221a42f9caa7bf82e841b8c9e3eaa
Quirin <quirin.ecker@bajupa.com>
2019-10-09 09:54:21 +0200

    home animation fixed

--- Application/scripts/homemenuAnimation.js
+++ Application/scripts/homemenuAnimation.js
@@ -2,14 +2,18 @@ function initialEventListener() {
     let homeButton = document.querySelector('footer');
 
     homeButton.addEventListener('click', () => {
-        console.log('aspdfpa')
         let animation = document.querySelector('#home-open-animation');
+        let home = document.querySelector('#home');
+        let animationContainer = document.querySelector('#home-open-animation-container');
+        let main = document.querySelector('main');
 
         animation.style.transform = 'scale(300)';
-        setTimeout(() => {
-
-        }, 1000)
-    })
+        animationContainer.style.overflow = 'hidden';
+        main.style.display = 'none';
+        home.style.transitionDelay = '1s';
+        home.style.display = 'block';
+        home.style.opacity = '1';
+    });
 }
 
 window.addEventListener('load', () => {
