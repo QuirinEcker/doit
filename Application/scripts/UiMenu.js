0ae7810b9bbf2671290179208d05b7c9818c820a
Quirin <quirin.ecker@bajupa.com>
2019-10-13 01:48:28 +0200

    removed redundant switchTo method in UiMenu

--- Application/scripts/UiMenu.js
+++ Application/scripts/UiMenu.js
@@ -5,16 +5,6 @@ class UiMenu {
         this.name = name;
         this.htmlElement = htmlElement;
     }
-
-    switchTo(uiMenu) {
-        uiMenu.htmlElement.style.display = 'flex';
-        this.htmlElement.style.opacity = '0';
-        setTimeout(() => {
-            this.htmlElement.style.display = 'none';
-            uiMenu.htmlElement.style.opacity = '1';
-            circleAnimation('black');
-        }, 10)
-    }
 }
 
 export {UiMenu};
