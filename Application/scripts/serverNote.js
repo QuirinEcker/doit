82ddb28461dc78b19ef73cd62ab8bf94a8c7a795
Quirin <quirin.ecker@bajupa.com>
2019-10-13 12:45:43 +0200

    added Webserver warning

---
+++ Application/scripts/serverNote.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    alert(`Die Website wird nicht funktionieren wenn sie nciht auf einen Server lauft.
+    Wenn python installiert ist kann man einen Webserver mit (python -m SimpleHTTPServer {Port}) anstarten.
+    Es wird auch mit jedem anderen Webserver funktionieren.
+    `);
+});
