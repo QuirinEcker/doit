69d8d5b0bb2b4356caa35ca17524ef273533255f
Quirin <quirin.ecker@bajupa.com>
2019-10-26 12:47:07 +0200

    added missing imports in DataBase.js

--- Application/scripts/DataBase.js
+++ Application/scripts/DataBase.js
@@ -1,3 +1,8 @@
+import {User} from "./EntityClasses/User.js";
+import {TaskList} from "./EntityClasses/TaskList.js";
+import {Task} from "./EntityClasses/Task.js";
+import {Tag} from "./EntityClasses/Tag.js";
+
 class DataBase {
     constructor(address) {
         this.adress = address
@@ -11,10 +16,14 @@ class DataBase {
         user1.addTaskList(taskList1OfUser1);
         user1.addTaskList(taskList2OfUser1);
         user1.addTaskList(taskList3OfUser1);
+        taskList1OfUser1.taskList.push(new Task('homework', Date.now(), taskList1OfUser1));
+        taskList2OfUser1.taskList.push(new Task('homework', Date.now(), taskList2OfUser1));
+        taskList3OfUser1.taskList.push(new Task('homework', Date.now(), taskList3OfUser1));
 
         return [
             user1
         ]
     }
-
 }
+
+export {DataBase}
