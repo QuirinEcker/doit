115a35c27b79200f9d2256d7b4b6d547f4818c3a
Quirin <quirin.ecker@bajupa.com>
2019-10-28 10:46:50 +0100

    improved searcher

--- Application/scripts/Searcher.js
+++ Application/scripts/Searcher.js
@@ -3,7 +3,22 @@ class Searcher {
     static search(searchString) {
         let openTasks = document.querySelector('#open-tasks').children;
         let closedTasks = document.querySelector('#closed-tasks').children;
-        let tasks = new Array().concat(Array.from(openTasks), Array.from(closedTasks));
+        let suggestedTasks = document.querySelector('#suggested-tasks').children;
+        let tasks = new Array().concat(Array.from(openTasks), Array.from(closedTasks), Array.from(suggestedTasks));
+        let searchTask = document.querySelector('#search-task');
+        let searchTaskTitle = document.querySelector(`#${searchTask.id} .title`);
+        let weekDayBox = document.querySelector(`#${searchTask.id} .big-display span`);
+
+        weekDayBox.textContent = Array.from(searchString).filter((charAt) => {
+            return charAt != ' ';
+        })[0];
+
+        searchTaskTitle.textContent = searchString;
+        if (searchString !== '') {
+            searchTask.style.display = 'flex';
+        } else {
+            searchTask.style.display = 'none';
+        }
 
         tasks.forEach((task) => {
             let title = document.querySelector(`#${task.id} .title`).textContent;
@@ -15,8 +30,6 @@ class Searcher {
                 task.style.display = 'none';
             }
         });
-
-        console.log(tasks)
     }
 }
 
