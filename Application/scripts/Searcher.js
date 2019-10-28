236c64766846bb3c16c97f61b8ca569cc7b58aed
Quirin <quirin.ecker@bajupa.com>
2019-10-28 11:10:45 +0100

    simplified array concat and changed == to ===

--- Application/scripts/Searcher.js
+++ Application/scripts/Searcher.js
@@ -4,23 +4,23 @@ class Searcher {
         let openTasks = document.querySelector('#open-tasks').children;
         let closedTasks = document.querySelector('#closed-tasks').children;
         let suggestedTasks = document.querySelector('#suggested-tasks').children;
-        let tasks = new Array().concat(Array.from(openTasks), Array.from(closedTasks), Array.from(suggestedTasks));
+        let tasks = [].concat(Array.from(openTasks), Array.from(closedTasks), Array.from(suggestedTasks));
         let searchTask = document.querySelector('#search-task');
         let searchTaskTitle = document.querySelector(`#${searchTask.id} .title`);
         let weekDayBox = document.querySelector(`#${searchTask.id} .big-display span`);
 
         let allLettersNotEmpty = Array.from(searchString).filter((charAt) => {
-            return charAt != ' ';
+            return charAt !== ' ';
         });
 
-        if (allLettersNotEmpty.length != 0) weekDayBox.textContent = allLettersNotEmpty[0].toUpperCase();
+        if (allLettersNotEmpty.length !== 0) weekDayBox.textContent = allLettersNotEmpty[0].toUpperCase();
         else weekDayBox.textContent = ' ';
 
 
         searchTaskTitle.textContent = searchString;
-        if (searchString.length != 0) {
+        if (searchString.length !== 0) {
             searchTask.style.display = 'flex';
-        } else if (searchString.length == 0) {
+        } else if (searchString.length === 0) {
             searchTask.style.display = 'none';
         }
 
