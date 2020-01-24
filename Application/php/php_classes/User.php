57baf96057a8d24455fd939529089b651513fbf1
Quirin <quirin.ecker@bajupa.com>
2020-01-25 00:38:26 +0100

    implemented php class user

--- Application/php/php_classes/User.php
+++ Application/php/php_classes/User.php
@@ -3,5 +3,18 @@
 
 class User
 {
+    private $id;
+    private $taskLists = array();
+    private $username;
+    private $email;
+    private $password;
+
+    public function __construct($id, $username, $email, $password)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->email = $email;
+        $this->password = $password;
+    }
 
 }
