c3777b3d5f97a0a8f68c971eac37f8542454eb11
Quirin <quirin.ecker@bajupa.com>
2020-01-25 00:27:40 +0100

    implemeted task php class

--- Application/php/php_classes/Task.php
+++ Application/php/php_classes/Task.php
@@ -4,6 +4,54 @@
 
 class Task
 {
+    private $id;
+    private $tags;
+    private $name;
+    private $dueDate;
+    private $parentTaskListID;
+    private $state;
+
+    public function __construct($id, $tags, $name, $dueDate, $parentTaskListID, $state)
+    {
+        $this->id = $id;
+        $this->tags = $tags;
+        $this->name = $name;
+        $this->dueDate = $dueDate;
+        $this->parentTaskListID = $parentTaskListID;
+        $this->state = $state;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getDueDate()
+    {
+        return $this->dueDate;
+    }
+
+    public function getParentTaskListID()
+    {
+        return $this->parentTaskListID;
+    }
+
+    public function getState()
+    {
+        return $this->state;
+    }
+
+
 
 
 }
