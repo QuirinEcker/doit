0541cbea90be04f664da9111c594c5ee77589352
Quirin <quirin.ecker@bajupa.com>
2020-01-25 00:31:41 +0100

    implemented TaskList php class

--- Application/php/php_classes/TaskList.php
+++ Application/php/php_classes/TaskList.php
@@ -3,5 +3,15 @@
 
 class TaskList
 {
+    private $id;
+    private $name;
+    private $tags = array();
+    private $tasks = array();
+
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
 
 }
