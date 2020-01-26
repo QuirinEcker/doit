8f4add24778e5a4078013a71855faf4d85726c2d
Quirin <quirin.ecker@bajupa.com>
2020-01-26 16:01:58 +0100

    data gets inserted, database class implemented

---
+++ Application/php/DataBase.php
@@ -0,0 +1,40 @@
+<?php
+
+
+class DataBase
+{
+
+    private $address;
+    private $user;
+    private $pw;
+    private $db;
+
+    function __construct($address, $user, $pw, $db)
+    {
+        $this->address = $address;
+        $this->user = $user;
+        $this->pw = $pw;
+        $this->db = $db;
+    }
+
+    public function save($object) {
+        $currentUsers = $this->getUsers();
+        $currentUsers[] = $object;
+
+        $file = fopen($this->address, "w") or die("Unable to open file");
+
+        fwrite($file, json_encode($currentUsers));
+        fclose($file);
+
+    }
+
+    public function getUsers() {
+        $address = $this->address;
+        $file = fopen($this->address, "r") or die("Unable to open file");
+        $users = json_decode(fread($file,filesize($this->address)));
+        var_dump($users);
+        fclose($file);
+
+        return $users;
+    }
+}
