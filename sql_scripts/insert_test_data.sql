8c714fb00882f8e0a613ffc9f647f455e7242056
Quirin <quirin.ecker@bajupa.com>
2020-03-23 21:59:13 +0100

    created script for inserting the test data

---
+++ sql_scripts/insert_test_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO USER (USERNAME, EMAIL, PASSWORD)
+    VALUES ('user0', 'user0@gmail.com', 'pw0'),
+           ('user1', 'user1@gmail.com', 'pw1'),
+           ('user2', 'user2@gmail.com', 'pw3');
+
+INSERT INTO TASK_LIST (NAME, USER_ID)
+    VALUES ('taskList0', 'user0@gmail.com'),
+           ('taskList1', 'user0@gmail.com'),
+           ('taskList2', 'user1@gmail.com'),
+           ('taskList3', 'user1@gmail.com'),
+           ('taskList4', 'user2@gmail.com'),
+           ('taskList5', 'user2@gmail.com');
