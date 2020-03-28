e2738550b6d1930f74fc336e8ad1b6aa10c6a872
Quirin <quirin.ecker@bajupa.com>
2020-03-28 03:19:23 +0100

    login is not static anymore

--- php/login.php
+++ php/login.php
@@ -1,7 +1,7 @@
 <?php
-include "DataBase.php";
+include "controller/DataBase.php";
 
 $username = explode(";", $_POST["val"])[0];
 $password = explode(";", $_POST["val"])[1];
 
-echo json_encode(DataBase::getInstance()->login("USER0@GMAIL.COM", "PW0"));
+echo json_encode(DataBase::getInstance()->login($username, $password));
