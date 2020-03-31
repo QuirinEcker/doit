bc6fd6de1870e36e3dd361872d42d5f6010de31c
Quirin <quirin.ecker@bajupa.com>
2020-03-31 14:49:54 +0200

    added backlink from tags to tasks

--- js_scripts/js_classes/Tag.js
+++ js_scripts/js_classes/Tag.js
@@ -2,15 +2,18 @@ import {Model} from "./Model.js";
 
 class Tag extends Model{
 
-    constructor(id = 0, name = "") {
+    constructor(id = 0, name = "", taskListID = 0) {
         super();
         this.id = id;
         this.name = name;
+        this.taskListID = taskListID;
+        this.tasks = [];
     }
 
     import(json) {
         this.id = json.id;
         this.name = json.name;
+        this.taskListID = json.taskListID;
     }
 }
 
