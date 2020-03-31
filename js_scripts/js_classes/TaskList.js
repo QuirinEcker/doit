248f16d5524a0c952a0acc6174d596d1b513248f
Quirin <quirin.ecker@bajupa.com>
2020-03-31 09:29:29 +0200

    implemented import for the lists in tasklist not entirely working

--- js_scripts/js_classes/TaskList.js
+++ js_scripts/js_classes/TaskList.js
@@ -1,4 +1,6 @@
 import {Model} from "./Model.js";
+import {Task} from "./Task.js";
+import {Tag} from "./Tag.js";
 
 class TaskList extends Model{
     constructor(id, name) {
@@ -12,6 +14,22 @@ class TaskList extends Model{
     import(json) {
         this.id = json.id;
         this.name = json.name;
+
+        if (json.tasks != null) {
+            json.tasks.forEach(task => {
+                let taskObject = new Task();
+                taskObject.import(task)
+                this.tasks[taskObject.id] = taskObject;
+            })
+        }
+
+        if (json.tags != null) {
+            json.tags.forEach(tag => {
+                let tagObject = new Tag();
+                tagObject.import(tag);
+                this.tags[tagObject.id] = tagObject;
+            })
+        }
     }
 }
 
