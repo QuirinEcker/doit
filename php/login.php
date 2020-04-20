2be1c8a1f7f392ffa593d0bd57e9acb8e7d886f9
Quirin <quirin.ecker@bajupa.com>
2020-04-20 17:58:56 +0200

    renamed field from username into email

--- php/login.php
+++ php/login.php
@@ -1,7 +1,7 @@
 <?php
 include "controller/UserRepository.php";
 
-$username = $_REQUEST["username"];
+$username = $_REQUEST["email"];
 $password = $_REQUEST["password"];
 
 echo json_encode(UserRepository::getInstance()->login($username, $password));
