170aae300eef0ec358be8cd182d87ebeba7545bb
Quirin <quirin.ecker@bajupa.com>
2020-05-10 02:18:48 +0200

    updated insert statements db name is now in the statement for inserting with the root user

--- sql_scripts/insert_test_data.sql
+++ sql_scripts/insert_test_data.sql
@@ -1,9 +1,9 @@
-INSERT INTO USER (USERNAME, EMAIL, PASSWORD)
+INSERT INTO doit_db.USER (USERNAME, EMAIL, PASSWORD)
     VALUES ('user0', 'user0@gmail.com', 'pw0'),
            ('user1', 'user1@gmail.com', 'pw1'),
            ('user2', 'user2@gmail.com', 'pw3');
 
-INSERT INTO TASK_LIST (NAME, USER_ID)
+INSERT INTO doit_db.TASK_LIST (NAME, USER_ID)
     VALUES ('taskList0', 'user0@gmail.com'),
            ('taskList1', 'user0@gmail.com'),
            ('taskList2', 'user1@gmail.com'),
