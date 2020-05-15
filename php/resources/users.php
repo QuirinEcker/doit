195719c732fe41d34621748e2a34241fbb53fa16
Quirin <quirin.ecker@bajupa.com>
2020-05-15 11:46:17 +0200

    implemented new method

--- php/resources/users.php
+++ php/resources/users.php
@@ -1,13 +1,16 @@
 <?php
 
 require_once "../controller/UserRepository.php";
+require_once "../controller/RequestData.php";
+
+$body = RequestData::getInstance()->getBody();
 
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "GET":
         echo json_encode(UserRepository::getInstance()->getUser());
         break;
     case "POST":
-        echo json_encode(UserRepository::getInstance()->create(json_decode($_REQUEST["data"])));
+        echo json_encode(UserRepository::getInstance()->create($body));
         break;
     case "DELETE":
         echo json_encode(UserRepository::getInstance()->delete());
