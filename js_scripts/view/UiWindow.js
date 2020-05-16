a7b45326051b265680f98e62511c2e4dbb6d0400
Quirin <quirin.ecker@bajupa.com>
2020-05-16 03:27:26 +0200

    implemented UiWindow class (WIP)

---
+++ js_scripts/view/UiWindow.js
@@ -0,0 +1,14 @@
+export class UiWindow {
+    constructor(window, htmlElement) {
+        this.name = name;
+        this.htmlElement = htmlElement;
+    }
+
+    open() {
+
+    }
+
+    close() {
+
+    }
+}
