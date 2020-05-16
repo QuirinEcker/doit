778e12b22cfc481deb2926c423df3a60c4012382
Quirin <quirin.ecker@bajupa.com>
2020-05-16 04:14:05 +0200

    new open script for opening the window

--- js_scripts/view/UiWindow.js
+++ js_scripts/view/UiWindow.js
@@ -1,12 +1,22 @@
 export class UiWindow {
-    constructor(name, htmlElement) {
+    constructor(name, htmlElement, height) {
         this.name = name;
         this.htmlElement = htmlElement;
         this.initiateEventHandler();
+        this.height = height;
     }
 
     open() {
+        const windowBackground = this.htmlElement.children[0];
+        const windowShape = this.htmlElement.children[1];
 
+        this.htmlElement.style.display = 'flex'
+
+        setTimeout(() => {
+            windowShape.style.width = '50%';
+            windowShape.style.height = this.height;
+            windowBackground.style.backgroundColor = 'rgba(0,0,0,0.5)'
+        }, 500)
     }
 
     close() {
