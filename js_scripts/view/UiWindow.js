055bfabc2a3bf8ab807228f4d9e4c453e80c25cd
Quirin <quirin.ecker@bajupa.com>
2020-05-16 04:05:08 +0200

    closing works

--- js_scripts/view/UiWindow.js
+++ js_scripts/view/UiWindow.js
@@ -2,6 +2,7 @@ export class UiWindow {
     constructor(name, htmlElement) {
         this.name = name;
         this.htmlElement = htmlElement;
+        this.initiateEventHandler();
     }
 
     open() {
@@ -9,6 +10,21 @@ export class UiWindow {
     }
 
     close() {
+        const windowBackground = this.htmlElement.children[0];
+        const windowShape = this.htmlElement.children[1];
 
+        windowShape.style.width = '0';
+        windowShape.style.height = '0';
+        windowBackground.style.backgroundColor = 'rgba(0,0,0,0.0)'
+
+        setTimeout(() => {
+            this.htmlElement.style.display = 'none'
+        }, 500)
+    }
+
+    initiateEventHandler() {
+        console.log(this.htmlElement);
+        const windowBackground = this.htmlElement.children[0];
+        windowBackground.addEventListener('click', () => this.close());
     }
 }
