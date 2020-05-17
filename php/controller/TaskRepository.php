ec00dfb7a7d228861c9670bcfbc932cf1ca07134
Quirin <quirin.ecker@bajupa.com>
2020-05-17 03:24:35 +0200

    created TaskRepository.php

---
+++ php/controller/TaskRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . "/ConnectionFactory.php";
+require_once __DIR__ . "/SessionController.php";
+require_once __DIR__ . "/SqlRunner.php";
+require_once __DIR__ . "/../model/Task.php";
+
+class TaskRepository
+{
+    static private $instance;
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new UserRepository();
+        }
+
+        return self::$instance;
+    }
+}
