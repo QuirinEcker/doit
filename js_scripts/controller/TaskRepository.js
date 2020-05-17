bda81124dba295368f30895cc7ccf13a50dcbc2d
Quirin <quirin.ecker@bajupa.com>
2020-05-17 04:16:04 +0200

    the fetch promise will now be returned

--- js_scripts/controller/TaskRepository.js
+++ js_scripts/controller/TaskRepository.js
@@ -3,7 +3,7 @@ import {Config} from "../view/Config.js";
 export class TaskRepository {
 
     getAll(taskListId) {
-        Config.dataBase.fetch(`./php/resources/tasks.php?id=${taskListId}`, 'GET')
+        return Config.dataBase.fetch(`./php/resources/tasks.php?id=${taskListId}`, 'GET')
             .then(response => response.json())
     }
 }
